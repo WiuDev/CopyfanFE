faf6fd5ff523fc2d3a61454ec197656cb14616f8
Willian Eduardo <willianeduardo9877@hotmail.com>
2025-10-16 12:58:03 -0300

    feat: adicionado .env

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,11 @@
+// babel.config.js - CORRETO!
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
-  plugins: ['react-native-reanimated/plugin'],
+  plugins: [
+    ['module:react-native-dotenv', {
+      'moduleName': '@env',
+      'path': '.env',
+    }],
+    'react-native-reanimated/plugin'
+  ],
 };
--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
-
+import {API_BASE_URL} from '@env';
 const api = axios.create({
-    baseURL: 'http://192.168.1.8:3000',
+    baseURL: API_BASE_URL,
 });
 
 export default api;
